7d31d28657eb2e716716f1c5e93321b355c6fb5c
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-11-11 07:56:01 -0300

    reply

--- sandbox/two_messages.py
+++
@@ -1,7 +0,0 @@
-from twilio.twiml.messaging_response import Message, MessagingResponse
-
-response = MessagingResponse()
-response.message('This is message 1 of 2.')
-response.message('This is message 2 of 2.')
-
-print(response)
