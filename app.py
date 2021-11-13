5f66f442095fdbc537caeddc0f383acf1c565fd4
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-11-12 21:47:38 -0300

    fix order

--- app.py
+++ app.py
@@ -31,23 +31,25 @@ def reply():
     # find chat or create a new one
     chat = db.find_one(filter={'from': incoming_from})
     if not chat:
-        chat = {'status': 1, 'from': incoming_from, 'name': incoming_msg}
-        db.insert([chat])
         response_msg.body(f'HoHoHo, aqui é o Papai Noel, qual é o sei nome?')
+        chat = {'status': 1, 'from': incoming_from}
+        db.insert([chat])
         return str(resp)
 
     status = chat['status']
 
     if status == 1:
+        db.update_one(incoming_from, 'name', incoming_msg, status)
         response_msg.body(f'E, aonde você mora?')
-        db.update_one(incoming_from, 'address', incoming_msg, status)
 
     elif status == 2:
+        db.update_one(incoming_from, 'address', incoming_msg, status)
         response_msg.body(f'Qual presente vc gostaria de ganhar do Papai Noel?')
-        db.update_one(incoming_from, 'gift', incoming_msg, status)
 
     elif status == 3:
+        db.update_one(incoming_from, 'gift', incoming_msg, status)
         response_msg.body(f'Continue se comportando e vc pode ganhar uma surpresa. Feliz Natal!!!')
+    else:
         db.update_one(incoming_from, 'last_message', incoming_msg, status)
 
     return str(resp)
